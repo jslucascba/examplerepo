cfadc48d598001372b854080e3108d8d48ea43a4
Jeff Lucas <jlucas13@eil-server.cba.ua.edu>
2020-09-02 07:03:02 -0500

    updated welcome message

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace examplereo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Git is great");
         }
     }
 }
